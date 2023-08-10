866fd3b692f33687e4f224310d8e117965eb4578
Fadli <55768134+bitchqueenoftheuniverse@users.noreply.github.com>
2023-08-10 01:22:43 -0400

    removed the comments, gone to the README

--- TurtleHardcoreMessages.lua
+++ TurtleHardcoreMessages.lua
@@ -4,13 +4,6 @@ local Original_ChatFrame_OnEvent = ChatFrame_OnEvent
 function ChatFrame_OnEvent(event)
   if event=='CHAT_MSG_SYSTEM' and arg1 then
     local name, level, init
-    --Anyname has reached level 10 in Hardcore mode! Their ascendance towards immortality continues, however, so do the dangers they will face.
-    --Anyname has reached level 20 in Hardcore mode! Their ascendance towards immortality continues, however, so do the dangers they will face.
-    --Anyname has reached level 30 in Hardcore mode! Their ascendance towards immortality continues, however, so do the dangers they will face.
-    --Anyname has reached level 40 in Hardcore mode! Their ascendance towards immortality continues, however, so do the dangers they will face.
-    --Anyname has reached level 50 in Hardcore mode! Their ascendance towards immortality continues, however, so do the dangers they will face.
-    --Anyname has transcended death and reached level 60 on Hardcore mode without dying once! Anyname shall henceforth be known as the Immortal!
-    --Anyname has laughed in the face of death in the Hardcore challenge. Anyname has begun the Inferno Challenge!
     if strfind(arg1, 'Hardcore mode') or strfind(arg1, 'Hardcore challenge') then
       if strfind(arg1, 'ascendance towards') then
         name = nameToLink(strsub(arg1, 1, strlen(arg1) - 130)) --name acquired
@@ -29,13 +22,6 @@ function ChatFrame_OnEvent(event)
       end
       
     elseif strfind(arg1, 'A tragedy has occurred.') then
-      --A tragedy has occurred. Hardcore character Any has fallen to NPCName (level 40) at level 1. May this sacrifice not be forgotten.
-      --A tragedy has occurred. Hardcore character Any has fallen in PvP to Enemyname at level 20. May this sacrifice not be forgotten.
-      --A tragedy has occurred. Hardcore character Any died of natural causes at level 7. May this sacrifice not be forgotten.
-
-      --A tragedy has occurred. Inferno character has fallen to NPCName (level 63) at level 60. They laughed in the face of death, but have learnt that death always has the last laugh.
-      --A tragedy has occurred. Inferno character has fallen in PvP to Enemyname at level 60. They laughed in the face of death, but have learnt that death always has the last laugh.
-      --A tragedy has occurred. Inferno character died of natural causes at level 60. They laughed in the face of death, but have learnt that death always has the last laugh.
       _, init = strfind(arg1, 'at level ')
       level = strsub(arg1, init + 1, strfind(arg1, '%. May') - 1) --level acquired
       
